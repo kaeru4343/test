d9b9e87bdd52b91dcccf68918c06b4d1844b2c14
kaeru4343 <k.furusawa@student.rug.nl>
2018-10-17 20:13:50 +0200

    Update repro.c

---
+++ repro.c
@@ -0,0 +1,20 @@
+/* file : repro.c */
+/* author : kenichi furusawa */ 
+/* date : 7-9-2016 */
+/* version: 1.0 */
+/* Description:a program that compute a program that computes how many packs of paper are needed for printing the readers. */
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+int x;
+scanf("%d", &x);
+if (x%500==0){
+	printf ("%d\n",x/500);
+}else{ 
+	printf("%d\n",x/500+1);
+}
+return 0;
+}
